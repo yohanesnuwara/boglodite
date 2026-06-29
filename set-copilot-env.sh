e51dbc31e430f4bc7f2650a884f1add8a811a37f
Yohanes Nuwara <ign.nuwara97@gmail.com>
2026-06-29 22:02:08 +0200

    shell script to use local model in copilot

---
+++ set-copilot-env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export COPILOT_PROVIDER_BASE_URL="http://192.168.56.1:1234/v1"
+export COPILOT_MODEL="ornith-1.0-9b"
+export COPILOT_OFFLINE="true"
+echo "Copilot env vars loaded."
